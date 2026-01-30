61757ef1c1a923cdb1ce2cc1e581bf010498a2b1
kiana <kianakarimi8383@gmail.com>
2026-01-30 12:24:05 -0800

    .

--- server.js
+++ server.js
@@ -14,27 +14,30 @@ class Server {
     start() {
         const server = http.createServer((req, res) => {
             const parsedUrl = url.parse(req.url, true);
+            const pathName = parsedUrl.pathname;
 
             // PART B
-            if (parsedUrl.pathname === '/COMP4537/labs/3/getDate') {
+            if (pathName.startsWith('/COMP4537/labs/3/getDate')) {
                 const name = parsedUrl.query.name || 'Guest';
                 const date = this.Utils.getDate();
 
                 res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.end(Messages.greeting(name, date));
+                return;
             }
 
             // C.1 — writeFile
-            else if (parsedUrl.pathname.startsWith('/COMP4537/labs/3/writeFile')) {
+            if (pathName.startsWith('/COMP4537/labs/3/writeFile')) {
                 const text = parsedUrl.query.text || '';
                 this.fileService.append(text);
 
                 res.writeHead(200, { 'Content-Type': 'text/plain' });
                 res.end('Text appended successfully');
+                return;
             }
 
             // C.2 — readFile
-            else if (parsedUrl.pathname.startsWith('/COMP4537/labs/3/readFile')) {
+            if (pathName.startsWith('/COMP4537/labs/3/readFile')) {
                 const content = this.fileService.read();
 
                 if (content === null) {
@@ -44,17 +47,16 @@ class Server {
                     res.writeHead(200, { 'Content-Type': 'text/plain' });
                     res.end(content);
                 }
+                return;
             }
 
-            else {
-                res.writeHead(404);
-                res.end('Not Found');
-            }
+            res.writeHead(404);
+            res.end('Not Found');
         });
 
-        const PORT = process.env.PORT || 3002;
+        const PORT = process.env.PORT || 3000;
         server.listen(PORT, () => {
-            console.log('Server running...');
+            console.log(`Server running on port ${PORT}`);
         });
     }
 }
